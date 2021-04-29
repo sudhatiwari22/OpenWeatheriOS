e3238ddac3f15b05207bd66dbe4ffabcccb1d7cc
Sudha <sudhatiwari22@gmail.com>
2021-04-29 20:13:31 +0530

    Created Xcode project for Open Weather API
    Created Xcode project for Open Weather API

---
+++ OpenWeatherApp/OpenWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Sudha Rani on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
